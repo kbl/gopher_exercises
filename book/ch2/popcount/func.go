62cbe1c030eea9c4e71d623759f66923bf26d82d
Marcin Pietraszek <m.pietraszek@gmail.com>
2018-04-24 23:17:01 +0200

    Popcount excercises

---
+++ book/ch2/popcount/func.go
@@ -0,0 +1,50 @@
+package popcount
+
+var pc [256]byte
+
+func init() {
+    for i := range pc {
+        pc[i] = pc[i/2] + byte(i & 1)
+    }
+}
+
+func PopCount(x uint64) int {
+    return int(pc[byte(x >> (0 * 8))] +
+        pc[byte(x >> (1 * 8))] +
+        pc[byte(x >> (2 * 8))] +
+        pc[byte(x >> (3 * 8))] +
+        pc[byte(x >> (4 * 8))] +
+        pc[byte(x >> (5 * 8))] +
+        pc[byte(x >> (6 * 8))] +
+        pc[byte(x >> (7 * 8))])
+}
+
+func PopCount8Loop(x uint64) int {
+    var count byte
+    var i uint64
+    for ; i < 8; i++ {
+        count += pc[byte(x >> (i * 8))]
+    }
+
+    return int(count)
+}
+
+func PopCount64Loop(x uint64) int {
+    var count int
+    for i := 0; i < 64; i++ {
+        x >>= 1
+        if x % 2 == 1 {
+            count++
+        }
+    }
+    return count
+}
+
+func PopCountMagic(x uint64) int {
+    var count int
+    for x != 0 {
+        x &= x - 1
+        count++
+    }
+    return count
+}
