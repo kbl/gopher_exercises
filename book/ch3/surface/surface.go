fc10272e473f198cbe26b7f5bd0604b00d6903e7
Marcin Pietraszek <m.pietraszek@gmail.com>
2018-05-03 23:36:08 +0200

    Adding colors to the svg drawing

--- book/ch3/surface/surface.go
+++ book/ch3/surface/surface.go
@@ -4,7 +4,6 @@ import (
     "fmt"
     "math"
     "io"
-    "os"
 )
 
 const (
@@ -14,6 +13,7 @@ const (
     xyscale = width / 2 / xyrange
     zscale = height * 0.4
     angle = math.Pi / 6
+    colorRange = 4 * 256
 )
 
 var sin30, cos30 = math.Sincos(angle)
@@ -24,24 +24,29 @@ func Write(out io.Writer, zTransform ZTransform) {
     fmt.Fprintf(out, "<svg xmlns='http://www.w3.org/2000/svg' " +
                "style='stroke: grey; fill: white; stroke-width: 0.7' " +
                "width='%d' height='%d'>", width, height)
+
+    zMin, zMax := findZRange(zTransform)
+
     for i := 0; i < cells; i++ {
         for j := 0; j < cells; j++ {
-            ax, ay := corner(i + 1, j, zTransform)
-            bx, by := corner(i, j, zTransform)
-            cx, cy := corner(i, j + 1, zTransform)
-            dx, dy := corner(i + 1, j + 1, zTransform)
+            ax, ay, az := corner(i + 1, j, zTransform)
+            bx, by, bz := corner(i, j, zTransform)
+            cx, cy, cz := corner(i, j + 1, zTransform)
+            dx, dy, dz := corner(i + 1, j + 1, zTransform)
+
+            color := findColor(zMin, zMax, (az + bz + cz + dz) / 4)
 
             if math.IsNaN(ax + ay + bx + by + cx + cy + dx + dy) {
                 continue
             }
 
-            fmt.Fprintf(out, "<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n", ax, ay, bx, by, cx, cy, dx, dy)
+            fmt.Fprintf(out, "<polygon points='%g,%g %g,%g %g,%g %g,%g' fill='%s'/>\n", ax, ay, bx, by, cx, cy, dx, dy, color)
         }
     }
     fmt.Fprintf(out, "</svg>\n")
 }
 
-func corner(i, j int, zTransform ZTransform) (float64, float64) {
+func corner(i, j int, zTransform ZTransform) (float64, float64, float64) {
     x := xyrange * (float64(i) / cells - 0.5)
     y := xyrange * (float64(j) / cells - 0.5)
 
@@ -50,7 +55,7 @@ func corner(i, j int, zTransform ZTransform) (float64, float64) {
     sx := width / 2 + (x - y) * cos30 * xyscale
     sy := height / 2 + (x + y) * sin30 * xyscale - z * zscale
 
-    return sx, sy
+    return sx, sy, z
 }
 
 func Peak(x, y float64) float64 {
@@ -68,3 +73,44 @@ func EggBox(x, y float64) float64 {
     return 0.1 * (math.Cos(x) + math.Cos(y))
 }
 
+func findColor(zMin, zMax, z float64) string {
+    z = (z - zMin) / (zMax - zMin)
+    color := int(math.Ceil(z * colorRange))
+    if color <= 255 {
+        return fmt.Sprintf("rgb(0, 0, %d)", color)
+    }
+    if color <= 255 * 2 {
+        color = color - 255
+        return fmt.Sprintf("rgb(0, %d, 255)", color)
+    }
+    if color <= 255 * 3 {
+        color = color - 255 * 2
+        return fmt.Sprintf("rgb(0, 255, %d)", 255 - color)
+    }
+    if color <= 255 * 4 {
+        color = color - 255 * 3
+        return fmt.Sprintf("rgb(%d, 255, 0)", color)
+    }
+    color = color - 255 * 4
+    return fmt.Sprintf("rgb(255, %d, 0)", 255 - color)
+}
+
+func findZRange(zTransform ZTransform) (float64, float64) {
+    zMin := math.MaxFloat64
+    zMax := -math.MaxFloat64
+    for x := -xyrange / 2; x <= xyrange / 2; x += 0.5 {
+        for y := -xyrange / 2; y <= xyrange / 2; y += 0.5 {
+            value := zTransform(x, y)
+            if math.IsNaN(value) {
+                continue
+            }
+            if value > zMax {
+                zMax = value
+            }
+            if value < zMin {
+                zMin = value
+            }
+        }
+    }
+    return zMin, zMax
+}
