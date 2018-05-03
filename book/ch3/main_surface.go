23b7015720ffaccc332e9b7a5ee17967ca370429
Marcin Pietraszek <m.pietraszek@gmail.com>
2018-05-03 21:33:13 +0200

    Surface can be written to io.Writer, skipping NaN values

---
+++ book/ch3/main_surface.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+    "book/ch3/surface"
+    "os"
+)
+
+func main() {
+    writeToStdout()
+}
+
+func writeToStdout() {
+    surface.Write(os.Stdout)
+}
--- book/ch3/surface/surface.go
+++ book/ch3/surface/surface.go
@@ -1,8 +1,9 @@
-package main
+package surface
 
 import (
     "fmt"
     "math"
+    "io"
 )
 
 const (
@@ -17,8 +18,8 @@ const (
 var sin30 = math.Sin(angle)
 var cos30 = math.Cos(angle)
 
-func main() {
-    fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' " +
+func Write(out io.Writer) {
+    fmt.Fprintf(out, "<svg xmlns='http://www.w3.org/2000/svg' " +
                "style='stroke: grey; fill: white; stroke-width: 0.7' " +
                "width='%d' height='%d'>", width, height)
     for i := 0; i < cells; i++ {
@@ -27,10 +28,15 @@ func main() {
             bx, by := corner(i, j)
             cx, cy := corner(i, j + 1)
             dx, dy := corner(i + 1, j + 1)
-            fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n", ax, ay, bx, by, cx, cy, dx, dy)
+
+            if math.IsNaN(ax + ay + bx + by + cx + cy + dx + dy) {
+                continue
+            }
+
+            fmt.Fprintf(out, "<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n", ax, ay, bx, by, cx, cy, dx, dy)
         }
     }
-    fmt.Println("</svg>")
+    fmt.Fprintf(out, "</svg>\n")
 }
 
 func corner(i, j int) (float64, float64) {
