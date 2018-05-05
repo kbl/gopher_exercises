20f0100aca42caf07d643c22e2b49ce7ea94af01
Marcin Pietraszek <m.pietraszek@gmail.com>
2018-05-05 12:16:00 +0200

    An attempt to add supersampling

--- book/ch3/mandelbrot/mandelbrot.go
+++ book/ch3/mandelbrot/mandelbrot.go
@@ -7,6 +7,7 @@ import (
     "math"
     "math/cmplx"
     "os"
+    "fmt"
 )
 
 const (
@@ -16,7 +17,10 @@ const (
     ymax   = 2
     width  = 1024
     height = 1024
+    xrange = xmax - xmin
+    yrange = ymax - ymin
 )
+
 
 var palette [256]color.Color
 
@@ -31,18 +35,18 @@ func init() {
 func Draw() {
     img := image.NewRGBA(image.Rect(0, 0, width, height))
     for py := 0; py < height; py++ {
-        y := float64(py) / height * (ymax - ymin) + ymin
+        y := float64(py) / height * yrange + ymin
         for px := 0; px < width; px++ {
-            x := float64(px) / width * (xmax - xmin) + xmin
-            z := complex(x, y)
-            img.Set(px, py, mandelbrot(z))
+            x := float64(px) / width * xrange + xmin
+            c := supersampled(x, y)
+            img.Set(px, py, c)
         }
     }
     png.Encode(os.Stdout, img)
 }
 
 func mandelbrot(z complex128) color.Color {
-    const iterations = 200
+    const iterations = len(palette)
 
     var v complex128
     for n := 0; n < iterations; n++ {
@@ -52,4 +56,53 @@ func mandelbrot(z complex128) color.Color {
         }
     }
     return color.Black
+}
+
+func supersampled(x, y float64) color.Color {
+    xDif := 0.5 / width * xrange
+    yDif := 0.5 / height * yrange
+
+    var colors []color.Color
+    for _, xx := range []float64 {-xDif, xDif} {
+        for _, yy := range []float64 {-yDif, yDif} {
+            z := complex(x + xx, y + yy)
+            c := mandelbrot(z)
+            colors = append(colors, c)
+        }
+    }
+
+    return avgColor(colors)
+}
+
+func avgColor(colors []color.Color) color.Color {
+    var r, g, b, a uint32
+
+    fmt.Println(r, g, b, a)
+
+    for _, c := range colors {
+        var rTemp, gTemp, bTemp, aTemp uint32 = c.RGBA()
+        var rTemp, gTemp, bTemp, aTemp uint32 = c.RGBA()
+        r += uint8(rTemp)
+        g += uint8(gTemp)
+        b += uint8(bTemp)
+        a += uint8(aTemp)
+        fmt.Println("c ", c)
+        fmt.Println("cr", r, rTemp, float64(rTemp), uint8(rTemp))
+        fmt.Println("cg", g, gTemp, float64(gTemp), uint8(rTemp))
+        fmt.Println("cb", b, bTemp, float64(bTemp), uint8(rTemp))
+        fmt.Println("ca", a, aTemp, float64(aTemp), uint8(rTemp))
+        fmt.Println()
+    }
+
+    fmt.Println("r", uint8(r / uint32(len(colors))), r / uint32(len(colors)))
+    fmt.Println("g", uint8(g / uint32(len(colors))), g / uint32(len(colors)))
+    fmt.Println("b", uint8(b / uint32(len(colors))), b / uint32(len(colors)))
+    fmt.Println("a", uint8(a / uint32(len(colors))), a / uint32(len(colors)))
+
+    return color.RGBA{
+        R: uint8(r / uint32(len(colors))),
+        G: uint8(g / uint32(len(colors))),
+        B: uint8(b / uint32(len(colors))),
+        A: uint8(a / uint32(len(colors))),
+    }
 }
---
+++ book/ch3/mandelbrot/mandelbrot_test.go
@@ -0,0 +1,22 @@
+package mandelbrot
+
+import (
+    "testing"
+    "image/color"
+)
+
+func TestAvgColors(t *testing.T) {
+    colors := []color.Color {
+        color.RGBA {R: uint8(35), G: uint8(35), B: uint8(220), A: uint8(255)},
+        color.Black,
+        color.Black,
+        color.Black,
+    }
+    expected := color.RGBA{R: uint8(8), G: uint8(8), B: uint8(55), A: uint8(255)}
+
+    avg := avgColor(colors)
+
+    if avg != expected {
+        t.Errorf("avgColor(%v) = %v, want %v", colors, avg, expected)
+    }
+}
