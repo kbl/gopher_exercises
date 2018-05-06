619eccdacf6fd998530e9375ddbd689dd0e3df01
Marcin Pietraszek <m.pietraszek@gmail.com>
2018-05-06 20:45:37 +0200

    Skipping excercise related to big.Rat

--- book/ch3/mandelbrot/mandelbrot.go
+++ book/ch3/mandelbrot/mandelbrot.go
@@ -93,30 +93,6 @@ func MandelbrotFloat(z complex128) color.Color {
     return color.Black
 }
 
-
-// type ComplexRat struct {
-//     r, i big.Rat
-// }
-//
-// func (c1 ComplexRat) Add(c2 ComplexRat) ComplexRat {
-//     return ComplexRat {
-//         c1.r + c2.r,
-//         c1.i + c2.i,
-//     }
-// }
-// 
-// func (c1 ComplexRat) Mul(c2 ComplexRat) ComplexRat {
-//     return ComplexRat {
-//         c1.r * c2.r - c1.i * c2.i,
-//         c1.r * c2.i + c1.i * c2.r,
-//     }
-// }
-// 
-// func (c ComplexRat) Abs() big.Rat {
-//     return sqrt(c.r * c.r + c.i * c.i)
-// }
-
-
 func Newtons(z complex128) color.Color {
     const iterations = 25
     const tolerance = 0.0001
