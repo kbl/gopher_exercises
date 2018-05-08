a2147fa62c8b474467b76573540f8ee786974c15
Marcin Pietraszek <m.pietraszek@gmail.com>
2018-05-08 19:26:00 +0200

    Comma nadling optional sign and floating point

--- book/ch03/comma/comma.go
+++ book/ch03/comma/comma.go
@@ -1,24 +1,50 @@
 package comma
 
-import "bytes"
+import (
+	"bytes"
+	"strings"
+)
+
+const (
+	separator        = ","
+	decimalDelimiter = "."
+	signs            = "+-"
+)
 
 func Comma(s string) string {
 	var b bytes.Buffer
+	var sign string
+	var decimalPart string
+
+	if strings.IndexAny(s, signs) == 0 {
+		sign = s[:1]
+		s = s[1:]
+	}
+
 	n := len(s)
+
 	if n <= 3 {
-		return s
+		return sign + s
+	}
+
+	if i := strings.Index(s, decimalDelimiter); i > 0 {
+		n = i
+		decimalPart = s[n:]
+		s = s[:n]
 	}
 
 	start := n % 3
 	if start == 0 {
 		start = 3
 	}
+	b.WriteString(sign)
 	b.WriteString(s[:start])
 
 	for i := start; i < n; i += 3 {
-		b.WriteString(",")
+		b.WriteString(separator)
 		b.WriteString(s[i : i+3])
 	}
+	b.WriteString(decimalPart)
 
 	return b.String()
 }
--- book/ch03/comma/comma_test.go
+++ book/ch03/comma/comma_test.go
@@ -20,3 +20,57 @@ func TestCommaRecursive(t *testing.T) {
 		}
 	}
 }
+
+func TestCommaRecursiveOptionalSign(t *testing.T) {
+	testCases := map[string]string{
+		"-123":     "-123",
+		"+1234":    "+1,234",
+		"-123456":  "-123,456",
+		"+1234567": "+1,234,567",
+	}
+
+	for testData, expected := range testCases {
+		result := Comma(testData)
+		if result != expected {
+			t.Errorf("Comma(%q) == %q, wants %q", testData, result, expected)
+		}
+	}
+}
+
+func TestCommaRecursiveFloatingPoint(t *testing.T) {
+	testCases := map[string]string{
+		"123.4":        "123.4",
+		"123.45":       "123.45",
+		"1234.5":       "1,234.5",
+		"1234.5678":    "1,234.5678",
+		"123456.7":     "123,456.7",
+		"1234567.8":    "1,234,567.8",
+		"1234567.8901": "1,234,567.8901",
+	}
+
+	for testData, expected := range testCases {
+		result := Comma(testData)
+		if result != expected {
+			t.Errorf("Comma(%q) == %q, wants %q", testData, result, expected)
+		}
+	}
+}
+
+func TestCommaRecursiveFloatingPointWithSign(t *testing.T) {
+	testCases := map[string]string{
+		"+123.4":        "+123.4",
+		"+123.45":       "+123.45",
+		"+1234.5":       "+1,234.5",
+		"-1234.5678":    "-1,234.5678",
+		"-123456.7":     "-123,456.7",
+		"-1234567.8":    "-1,234,567.8",
+		"-1234567.8901": "-1,234,567.8901",
+	}
+
+	for testData, expected := range testCases {
+		result := Comma(testData)
+		if result != expected {
+			t.Errorf("Comma(%q) == %q, wants %q", testData, result, expected)
+		}
+	}
+}
