f59625ce874b045cadb1870165ff3b1442495155
Marcin Pietraszek <m.pietraszek@gmail.com>
2018-05-08 18:58:47 +0200

    Comma using for loop

--- book/ch03/comma/comma.go
+++ book/ch03/comma/comma.go
@@ -1,9 +1,24 @@
 package comma
 
+import "bytes"
+
 func Comma(s string) string {
+	var b bytes.Buffer
 	n := len(s)
 	if n <= 3 {
 		return s
 	}
-	return Comma(s[:n-3]) + "," + s[n-3:]
+
+	start := n % 3
+	if start == 0 {
+		start = 3
+	}
+	b.WriteString(s[:start])
+
+	for i := start; i < n; i += 3 {
+		b.WriteString(",")
+		b.WriteString(s[i : i+3])
+	}
+
+	return b.String()
 }
