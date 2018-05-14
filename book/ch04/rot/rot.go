2f1c2389576373fdba2bebcb1a1119c070e0f417
Marcin Pietraszek <m.pietraszek@gmail.com>
2018-05-14 22:31:12 +0200

    rot without rev

--- book/ch04/rot/rot.go
+++ book/ch04/rot/rot.go
@@ -2,10 +2,31 @@ package rot
 
 import "book/ch04/rev"
 
-func Rot(s []int, by int) {
-	if by > len(s) {
+func RotWithRev(s []int, by int) {
+	if len(s) == 0 || by < 1 {
 		return
 	}
-	for i, v := range s {
+	by %= len(s)
+	rev.Rev(s[by:])
+	rev.Rev(s[:by])
+	rev.Rev(s)
+}
+
+func Rot(s []int, by int) {
+	if len(s) == 0 || by < 1 {
+		return
+	}
+
+	by %= len(s)
+	c := make([]int, by)
+	copy(c, s[:by])
+
+	var i int
+
+	for ; i < len(s)-by; i++ {
+		s[i] = s[(i+by)%len(s)]
+	}
+	for j, v := range c {
+		s[i+j] = v
 	}
 }
