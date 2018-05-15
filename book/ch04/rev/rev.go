3cc52c609f9b53ea0eaa3e2479ffb4b070795ca2
Marcin Pietraszek <m.pietraszek@gmail.com>
2018-05-15 20:42:00 +0200

    Reversing utf8 bytes

--- book/ch04/rev/rev.go
+++ book/ch04/rev/rev.go
@@ -1,13 +1,24 @@
 package rev
 
-func Rev(s []int) {
+import "unicode/utf8"
+
+func Reverse(s []byte) {
 	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
 		s[i], s[j] = s[j], s[i]
 	}
 }
 
-func RevArray(s *[4]int) {
+func ReverseArray(s *[4]int) {
 	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
 		s[i], s[j] = s[j], s[i]
 	}
+}
+
+func ReverseUtf8(s []byte) {
+	for i := 0; i < len(s); {
+		_, size := utf8.DecodeRune(s[i:])
+		Reverse(s[i : i+size])
+		i += size
+	}
+	Reverse(s)
 }
--- book/ch04/rev/rev_test.go
+++ book/ch04/rev/rev_test.go
@@ -5,57 +5,68 @@ import (
 	"testing"
 )
 
-func TestRevEmptySlice(t *testing.T) {
-	input := []int{}
-	before := []int{}
-	expected := []int{}
-	Rev(input)
+func TestReverseEmptySlice(t *testing.T) {
+	input := []byte{}
+	before := []byte{}
+	expected := []byte{}
+	Reverse(input)
 
 	if !reflect.DeepEqual(input, expected) {
-		t.Errorf("Rev(%v) = %v, want %v", before, input, expected)
+		t.Errorf("Reverse(%v) = %v, want %v", before, input, expected)
 	}
 }
 
-func TestRevNilSlice(t *testing.T) {
-	var input []int
-	var before []int
-	var expected []int
-	Rev(input)
+func TestReverseNilSlice(t *testing.T) {
+	var input []byte
+	var before []byte
+	var expected []byte
+	Reverse(input)
 
 	if !reflect.DeepEqual(input, expected) {
-		t.Errorf("Rev(%v) = %v, want %v", before, input, expected)
+		t.Errorf("Reverse(%v) = %v, want %v", before, input, expected)
 	}
 }
 
-func TestRevOddLengthSlice(t *testing.T) {
-	input := []int{1, 2, 3}
-	before := []int{1, 2, 3}
-	expected := []int{3, 2, 1}
-	Rev(input)
+func TestReverseOddLengthSlice(t *testing.T) {
+	input := []byte{1, 2, 3}
+	before := []byte{1, 2, 3}
+	expected := []byte{3, 2, 1}
+	Reverse(input)
 
 	if !reflect.DeepEqual(input, expected) {
-		t.Errorf("Rev(%v) = %v, want %v", before, input, expected)
+		t.Errorf("Reverse(%v) = %v, want %v", before, input, expected)
 	}
 }
 
-func TestRevEvenLengthSlice(t *testing.T) {
-	input := []int{1, 2, 3, 4}
-	before := []int{1, 2, 3, 4}
-	expected := []int{4, 3, 2, 1}
-	Rev(input)
+func TestReverseEvenLengthSlice(t *testing.T) {
+	input := []byte{1, 2, 3, 4}
+	before := []byte{1, 2, 3, 4}
+	expected := []byte{4, 3, 2, 1}
+	Reverse(input)
 
 	if !reflect.DeepEqual(input, expected) {
-		t.Errorf("Rev(%v) = %v, want %v", before, input, expected)
+		t.Errorf("Reverse(%v) = %v, want %v", before, input, expected)
 	}
 }
 
-func TestRevArrayEvenLengthSlice(t *testing.T) {
+func TestReverseArrayEvenLengthSlice(t *testing.T) {
 	input := [...]int{1, 2, 3, 4}
 	before := [...]int{1, 2, 3, 4}
 	expected := [...]int{4, 3, 2, 1}
-	RevArray(&input)
+	ReverseArray(&input)
 
 	if !reflect.DeepEqual(input, expected) {
-		t.Errorf("RevArray(%v) = %v, want %v", before, input, expected)
+		t.Errorf("ReverseArray(%v) = %v, want %v", before, input, expected)
+	}
+}
+
+func TestReverseUtf8(t *testing.T) {
+	input := []byte("zażółć gęślą jaźń")
+	before := []byte("zażółć gęślą jaźń")
+	expected := []byte("ńźaj ąlśęg ćłóżaz")
+	ReverseUtf8(input)
+
+	if !reflect.DeepEqual(input, expected) {
+		t.Errorf("ReverseUtf8(%q) = %q, want %q", before, input, expected)
 	}
 }
