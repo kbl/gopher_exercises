5dc9be7be3503d3f503ec13fff7b7624aa8f5faf
Marcin Pietraszek <m.pietraszek@gmail.com>
2018-05-17 21:53:32 +0200

    Reporting in age categories

--- book/ch04/main_github.go
+++ book/ch04/main_github.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 )
 
 func main() {
@@ -13,9 +14,40 @@ func main() {
 		log.Fatal(err)
 	}
 
+	now := time.Now()
+	issues := make(map[string][]*github.Issue)
+
+	lessThanMonth, err := time.ParseDuration(fmt.Sprintf("%dh", 30*24))
+	if err != nil {
+		log.Fatal(err)
+	}
+	lessThanYear, err := time.ParseDuration(fmt.Sprintf("%dh", 365*24))
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Printf("%d issues:\n", result.TotalCount)
 
 	for _, item := range result.Items {
-		fmt.Printf("#%-5d %9.9s %.55s\n", item.Number, item.User.Login, item.Title)
+		age := now.Sub(item.CreatedAt)
+		category := ">= 1y"
+		if age < lessThanMonth {
+			category = "< 30d"
+		} else if age < lessThanYear {
+			category = "< 1y"
+		}
+
+		if _, exists := issues[category]; !exists {
+			issues[category] = make([]*github.Issue, 0)
+		}
+
+		issues[category] = append(issues[category], item)
+	}
+
+	for _, category := range [...]string{"< 30d", "< 1y", ">= 1y"} {
+		fmt.Printf("%d issues %s:\n", len(issues[category]), category)
+		for _, item := range issues[category] {
+			fmt.Printf("#%-5d %9.9s %.55s\n", item.Number, item.User.Login, item.Title)
+		}
 	}
 }
