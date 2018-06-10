181499300f9889775de2a2f721ae4e8f8b9453ae
Marcin Pietraszek <m.pietraszek@gmail.com>
2018-06-10 16:27:32 +0200

    commandline program for downloading posters from omdbapi

--- book/ch04/omdb/omdb.go
+++ book/ch04/omdb/omdb.go
@@ -1,7 +1,43 @@
 package omdb
 
-const searchURLTemplate = "http://img.omdbapi.com/?apikey=%s&&t=%s"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+)
 
-func DownloadPoster(apiToke, movieTitle string) []byte {
-	return nil
+// posters api is only available for patrons
+const searchURLTemplate = "http://omdbapi.com/?apikey=%s&&t=%s"
+
+type movieResponse struct {
+	Poster string
+}
+
+func DownloadPoster(apiToken, movieTitle string) []byte {
+	url := fmt.Sprintf(searchURLTemplate, url.QueryEscape(apiToken), url.QueryEscape(movieTitle))
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	var posterPath movieResponse
+	if err := json.NewDecoder(resp.Body).Decode(&posterPath); err != nil {
+		log.Fatal(err)
+	}
+	if posterPath.Poster == "" {
+		log.Fatalf("Movie with title %q is missing!", movieTitle)
+	}
+
+	posterResponse, err := http.Get(posterPath.Poster)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer posterResponse.Body.Close()
+	var posterBuffer bytes.Buffer
+	posterBuffer.ReadFrom(posterResponse.Body)
+
+	return posterBuffer.Bytes()
 }
