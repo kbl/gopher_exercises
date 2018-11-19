d9aa61f024c4ef1aba0f64d6acd06a34fc7f48bc
Marcin Pietraszek <m.pietraszek@gmail.com>
2018-11-19 14:01:12 +0100

    Did I understand pointer receivers?

--- book/ch07/ex705/limit.go
+++ book/ch07/ex705/limit.go
@@ -6,29 +6,29 @@ import (
 
 type reader struct {
 	wrapped io.Reader
-	to_read *int64
+	to_read int64
 }
 
-func (r reader) Read(b []byte) (int, error) {
-	if *r.to_read == 0 {
+func (r *reader) Read(b []byte) (int, error) {
+	if r.to_read == 0 {
 		return 0, io.EOF
 	}
 
-	if *r.to_read < int64(len(b)) {
-		b = b[:*r.to_read]
+	if r.to_read < int64(len(b)) {
+		b = b[:r.to_read]
 	}
 
 	count, err := r.wrapped.Read(b)
 
-	*r.to_read -= int64(count)
+	r.to_read -= int64(count)
 	if err == io.EOF {
-		*r.to_read = 0
+		r.to_read = 0
 	}
 
 	return count, err
 }
 
 func LimitReader(r io.Reader, n int64) io.Reader {
-	lr := reader{r, &n}
-	return lr
+	lr := reader{r, n}
+	return &lr
 }
