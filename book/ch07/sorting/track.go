633e31f485b94335594ea002783177b1023912f3
Marcin Pietraszek <m.pietraszek@gmail.com>
2018-11-21 08:47:59 +0100

    Initial implementation for track stuct

---
+++ book/ch07/sorting/track.go
@@ -0,0 +1,49 @@
+package sorting
+
+import (
+	"fmt"
+	"os"
+	"text/tabwriter"
+	"time"
+)
+
+type Track struct {
+	Title,
+	Artist,
+	Album string
+	Year   int
+	Length time.Duration
+}
+
+func length(s string) time.Duration {
+	d, err := time.ParseDuration(s)
+	if err != nil {
+		panic(s)
+	}
+	return d
+}
+
+func printTracks(tracks []*Track) {
+	const format = "%v\t%v\t%v\t%v\t%v\t\n"
+	tw := new(tabwriter.Writer).Init(os.Stdout, 0, 8, 2, ' ', 0)
+	fmt.Fprintf(tw, format, "Title", "Artist", "Album", "Year", "Length")
+	fmt.Fprintf(tw, format, "-----", "------", "-----", "----", "------")
+	for _, t := range tracks {
+		fmt.Fprintf(tw, format, t.Title, t.Artist, t.Album, t.Year, t.Length)
+	}
+	tw.Flush()
+}
+
+type TrackSlice []*Track
+
+func (x TrackSlice) Len() int {
+	return len(x)
+}
+
+func (x TrackSlice) Less(i, j, int) bool {
+	return x[i].Artist < x[j].Artist
+}
+
+func (x TrackSlice) Swap(i, j int) {
+	x[i], x[j] = x[j], x[i]
+}
