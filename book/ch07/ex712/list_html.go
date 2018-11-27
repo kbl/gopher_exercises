8992cf6d4d5684fea471e275240d26988445cc69
Marcin Pietraszek <m.pietraszek@gmail.com>
2018-11-27 08:37:41 +0100

    Working solution for exercise 7.12

---
+++ book/ch07/ex712/list_html.go
@@ -0,0 +1,32 @@
+package ex712
+
+import (
+	"book/ch07/ex711"
+	"html/template"
+	"net/http"
+)
+
+var htmlTemplate = template.Must(template.New("x").Parse(`<html>
+<body>
+  <table>
+    <tr>
+      <th>Name</th>
+      <th>Price</th>
+    </tr>
+	{{ range $key, $value := . }}
+	<tr>
+      <td>{{$key}}</td>
+      <td>{{$value}}</td>
+	</tr>
+	{{ end}}
+  </table>
+  </body>
+</html>`))
+
+type Database struct {
+	ex711.Database
+}
+
+func (db Database) List(w http.ResponseWriter, req *http.Request) {
+	htmlTemplate.Execute(w, db.Database.Database)
+}
